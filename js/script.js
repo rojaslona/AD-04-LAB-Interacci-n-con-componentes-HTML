5d974384de8781199ced0c5d19b0adeb8e5efea7
rojaslona <rojaslona@outlook.com>
2025-09-18 10:56:48 -0600

    tasks finished

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function showBurgerAlert() {
+    alert("ES HORA DE UNA HAMBURGUESA");
+}
+
+function burgerTownClick() {
+    console.log("¡Alguien hizo clic en BURGER TOWN!");
+}
+
+function changeTitleColor() {
+    const title = document.querySelector("h2");
+    title.style.color = "red";
+}
